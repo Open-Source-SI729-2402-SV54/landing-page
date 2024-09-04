c5111f3e4247321b595dca798e95d2f737dde3bc
C7leo <149843920+C7leo@users.noreply.github.com>
2024-09-04 03:43:32 -0500

    main
    feat(landing page): added index, styles and main

--- main.js
+++ main.js
@@ -19,3 +19,70 @@ const scrollRevealOption = {
     origin: "bottom",
     duration: 1000,
 };
+
+ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header__content h2", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header__content .section__description", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".header__btns", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
+
+ScrollReveal().reveal(".about__section", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".section__header", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".experience__content", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".discover__card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
+
+
+ScrollReveal().reveal(".chef__image img", {
+    ...scrollRevealOption,
+    origin: "left",
+});
+ScrollReveal().reveal(".chef__content .section__subheader", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".chef__content .section__header", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".chef__content .section__description", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+
+
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+});
